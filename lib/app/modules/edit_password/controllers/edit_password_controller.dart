19a7e6200768546a8c9462f5ee060f0a7e2f5201
9 Muhammad Ovie Raka Siwi <muhammadovierakasiwi@gmail.com>
2023-01-02 13:33:47 +0700

    Edit Password Commit

--- lib/app/modules/edit_password/controllers/edit_password_controller.dart
+++ lib/app/modules/edit_password/controllers/edit_password_controller.dart
@@ -1,23 +1,52 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 
 class EditPasswordController extends GetxController {
-  //TODO: Implement EditPasswordController
+  TextEditingController currentPasswordController = TextEditingController();
+  TextEditingController newPasswordController = TextEditingController();
+  TextEditingController confirmNewPasswordController = TextEditingController();
 
-  final count = 0.obs;
-  @override
-  void onInit() {
-    super.onInit();
+  RxBool isLoading = false.obs;
+  var isCurrentPasswordHide = true.obs;
+  var isNewPassworddHide = true.obs;
+  var isConfirmNewPasswordHide = true.obs;
+  FirebaseAuth auth = FirebaseAuth.instance;
+
+  void editPassword() async {
+    if (currentPasswordController.text.isNotEmpty &&
+        newPasswordController.text.isNotEmpty &&
+        confirmNewPasswordController.text.isNotEmpty) {
+      if (newPasswordController.text == confirmNewPasswordController.text) {
+        isLoading.value = true;
+        try {
+          String emailUser = auth.currentUser!.email!;
+
+          await auth.signInWithEmailAndPassword(
+              email: emailUser, password: currentPasswordController.text);
+
+          await auth.currentUser!.updatePassword(newPasswordController.text);
+
+          Get.back();
+          Get.snackbar('Berhasil!', 'Anda berhasil mengganti pasword');
+        } on FirebaseAuthException catch (e) {
+          if (e.code == 'wrong_password') {
+            Get.snackbar('Terjadi Kesalahan',
+                'Password sekarang anda salah, masukkan dengan benar');
+          } else {
+            Get.snackbar('Terjadi Kesalahan', e.code.toLowerCase());
+          }
+        } catch (e) {
+          Get.snackbar('Terjadi Kesalahan',
+              'Tidak dapat mengganti password, silahkan hubungi operator');
+        } finally {
+          isLoading.value = false;
+        }
+      } else {
+        Get.snackbar('Terjadi Kesalahan', 'Konfirmasi password harus sama');
+      }
+    } else {
+      Get.snackbar('Terjadi Kesalahan', 'Password tidak boleh kosong');
+    }
   }
-
-  @override
-  void onReady() {
-    super.onReady();
-  }
-
-  @override
-  void onClose() {
-    super.onClose();
-  }
-
-  void increment() => count.value++;
 }
--- lib/app/modules/edit_password/views/edit_password_view.dart
+++ lib/app/modules/edit_password/views/edit_password_view.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 
+import '../../../data/theme/colors.dart';
+import '../../../data/theme/custom_widget.dart';
 import '../controllers/edit_password_controller.dart';
 
 class EditPasswordView extends GetView<EditPasswordController> {
@@ -9,15 +12,90 @@ class EditPasswordView extends GetView<EditPasswordController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[200],
       appBar: AppBar(
-        title: const Text('EditPasswordView'),
+        backgroundColor: softNavy,
+        title: Text("EDIT PASSWORD",
+            style: GoogleFonts.poppins(
+                textStyle: const TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600,
+                    color: softWhite))),
         centerTitle: true,
       ),
-      body: Center(
-        child: Text(
-          'EditPasswordView is working',
-          style: TextStyle(fontSize: 20),
-        ),
+      body: ListView(
+        padding: const EdgeInsets.all(20),
+        children: [
+          Text("Password Saat Ini",
+              style: GoogleFonts.poppins(
+                  textStyle: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w400,
+                      color: softNavy))),
+          const SizedBox(
+            height: 5,
+          ),
+          inputPassword(
+              controller: controller.currentPasswordController,
+              obscureText: controller.isCurrentPasswordHide,
+              onPressed: () {
+                controller.isCurrentPasswordHide.toggle();
+              },
+              isPasswordHide: controller.isCurrentPasswordHide,
+              hintText: ""),
+          const SizedBox(
+            height: 20,
+          ),
+          Text("Password Baru",
+              style: GoogleFonts.poppins(
+                  textStyle: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w400,
+                      color: softNavy))),
+          const SizedBox(
+            height: 5,
+          ),
+          inputPassword(
+              controller: controller.newPasswordController,
+              obscureText: controller.isNewPassworddHide,
+              onPressed: () {
+                controller.isNewPassworddHide.toggle();
+              },
+              isPasswordHide: controller.isNewPassworddHide,
+              hintText: ""),
+          const SizedBox(
+            height: 20,
+          ),
+          Text("Konfirmasi Password Baru",
+              style: GoogleFonts.poppins(
+                  textStyle: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w400,
+                      color: softNavy))),
+          const SizedBox(
+            height: 5,
+          ),
+          inputPassword(
+              controller: controller.confirmNewPasswordController,
+              obscureText: controller.isConfirmNewPasswordHide,
+              onPressed: () {
+                controller.isConfirmNewPasswordHide.toggle();
+              },
+              isPasswordHide: controller.isConfirmNewPasswordHide,
+              hintText: ""),
+          const SizedBox(
+            height: 30,
+          ),
+          Obx(() => basicButton(
+              onPressed: () async {
+                if (controller.isLoading.isFalse) {
+                  controller.editPassword();
+                }
+              },
+              buttonText: controller.isLoading.isFalse
+                  ? 'UBAH PASSWORD'
+                  : 'Loading...'))
+        ],
       ),
     );
   }
